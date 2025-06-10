25c7f9058e90690c28e03332f16a6aee4a733619
hrisoved <93000130+hrisoved@users.noreply.github.com>
2025-06-10 22:45:47 +1000

    fixed contact font size and about page link hover

--- personal-blog-master/components/ContactLink.js
+++ personal-blog-master/components/ContactLink.js
@@ -1,7 +1,7 @@
 const ContactLink = ({ title, href, icon }) => {
   return (
     <a href={href} target="_blank" rel="noopener noreferrer">
-      <li className="duration-250 mr-2 inline cursor-pointer text-2xl transition-colors ease-in hover:text-gray-400 sm:text-6xl md:text-3xl lg:text-6xl">
+      <li className="duration-250 mr-2 inline cursor-pointer text-3xl transition-colors ease-in hover:text-gray-400 sm:text-3xl md:text-3xl lg:text-2xl">
         <span className="duration-250 transition-colors ease-in hover:text-primary-500 dark:hover:text-primary-400">
           {icon}
         </span>
