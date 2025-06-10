0a75d0c57c47035d990cf7be8226bccc362809c7
hrisoved <93000130+hrisoved@users.noreply.github.com>
2025-06-10 15:19:05 +1000

    fix linting issues

--- personal-blog-master/pages/index.js
+++ personal-blog-master/pages/index.js
@@ -69,9 +69,17 @@ export default function Home({ posts }) {
                   // animationDelay={300}
                   animationDuration={800}
                 >
-                <span style={{ display: 'inline-block', maxWidth: '120px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-
-                  college years,</span>
+                  <span
+                    style={{
+                      display: 'inline-block',
+                      maxWidth: '120px',
+                      whiteSpace: 'nowrap',
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                    }}
+                  >
+                    college years,
+                  </span>
                 </RoughNotation>{' '}
                 <RoughNotation
                   type="underline"
