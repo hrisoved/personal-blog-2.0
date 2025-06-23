b7648ea91d796c1bb239781fa455d9d70f5a64c4
hrisoved <93000130+hrisoved@users.noreply.github.com>
2025-06-22 23:20:41 -0400

    deleted blog section on home page index.js and disbled blog section in navigation and header

--- personal-blog-master/data/nav.js
+++ personal-blog-master/data/nav.js
@@ -1,7 +1,7 @@
 export const navigation = {
   pages: [
     { name: 'Home', href: '/' },
-    { name: 'Blog', href: '/blog' },
+    // { name: 'Blog', href: '/blog' },
     { name: 'Projects', href: '/projects' },
     { name: 'About', href: '/about' },
     { name: 'Contact', href: '/contact' },
