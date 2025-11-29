da1bdd9f99ae74903491048a30dd1c3a579ed3a7
hrisoved <93000130+hrisoved@users.noreply.github.com>
2025-11-29 22:23:04 +0000

    lint and ontent updates

--- personal-blog-master/layouts/AuthorLayout.js
+++ personal-blog-master/layouts/AuthorLayout.js
@@ -85,8 +85,8 @@ export default function AuthorLayout({ children, frontMatter }) {
                       {' '}
                       University of Dublin, Trinity College
                     </Link>{' '}
-                    and I was awarded the Gold Medal for exceptional academic excellence in
-                    my undergraduate degree.
+                    and I was awarded the Gold Medal for exceptional academic excellence in my
+                    undergraduate degree.
                   </p>
                 </RoughNotation>
               </p>
